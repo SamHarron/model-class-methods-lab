f6c3a9e77d322eaf9367160cd8ff43335036cee7
SamHarron <harronsam@gmail.com>
2021-07-09 14:42:39 -0400

    commit tree 78fde1d0563b2471498331ec0a14b7370d8758d3

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -4,42 +4,42 @@ class Boat < ActiveRecord::Base
   has_many    :classifications, through: :boat_classifications
 
   def self.first_five
-    # all.limit(5)
+    all.limit(5) # returns the first 5 objects (Boats)
   end
 
   def self.dinghy
-    # where("length < 20")
+    where("length < 20") #returns boats shorter than 20 feet
   end
 
   def self.ship
-    # where("length >= 20")
+    where("length >= 20") #returns boats 20 feet or longer
   end
 
-  def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+  def self.last_three_alphabetically #returns last three boats in alphabetical order
+    all.order(name: :desc).limit(3)
   end
 
-  def self.without_a_captain
-    # where(captain_id: nil)
+  def self.without_a_captain #returns boats without a captain
+    where(captain_id: nil)
   end
 
-  def self.sailboats
-    # includes(:classifications).where(classifications: { name: 'Sailboat' })
+  def self.sailboats #returns all boats that are sailboats
+    includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
-  def self.with_three_classifications
+  def self.with_three_classifications #returns boats with three classifications
     # This is really complex! It's not common to write code like this
     # regularly. Just know that we can get this out of the database in
     # milliseconds whereas it would take whole seconds for Ruby to do the same.
     #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+    joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
-  def self.non_sailboats
-    # where("id NOT IN (?)", self.sailboats.pluck(:id))
+  def self.non_sailboats #returns all captains of catamarans
+    where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
-  def self.longest
-    # order('length DESC').first
+  def self.longest #
+    order('length DESC').first
   end
 end
